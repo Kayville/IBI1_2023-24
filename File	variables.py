7bec248c100b8fee49b428523c4a85fd9b3ce605
Kayville <141344847+Kayville@users.noreply.github.com>
2024-05-21 05:07:16 +0800

    Create File	variables.py

---
+++ File	variables.py
@@ -0,0 +1,16 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d>e
+   print(d)
+# running	training regime	had	a	greater	improvement	on	running	time
+if e>d
+   print(e)
+# running	and	strength	training regime	had	a	greater	improvement	on	running	time
+
+X=True
+Y=False
+W=X or Y
+# True
